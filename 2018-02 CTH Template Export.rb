c6aecd0ba907f90ab10092675d2d398ad482d3e2
JamesEmpireOne <james@empireone.com.au>
2018-02-28 14:19:33 +1100

    Add more output to CTH template export

--- 2018-02 CTH Template Export.rb
+++ 2018-02 CTH Template Export.rb
@@ -1,18 +1,19 @@
 Dir.glob("#{Rails.root}/lib/tasks/helpers/*.rb") {|file| require file}
 include RakeHelper
-
-session = RakeHelper::init_google_session
+Google::APIClient.logger.level = 2
+session = RakeHelper::init_google_session 53339
 sheet =  RakeHelper::init_google_sheet(nil, session)
 
-def print_member_orders comp, session, sheet
+def print_member_orders cid, session, sheet
 	export_sheet = RakeHelper::init_google_worksheet "Member Template Export", sheet
-	import_sheet = RakeHelper::init_google_worksheet "Member Template Import", sheet
+	import_sheet = RakeHelper::init_google_worksheet "Pending", sheet
 	comp = Company.find cid;
 	offset = 1
 	t_offset = 6
 	comp.customers.joins(:memberships).where(:memberships => {:status => 'current'}).order("memberships.membership_type_id ASC, customers.fname ASC, customers.lname ASC").each_with_index do |cus, cdx|
 		template = CompanyCustomer.template_order(cus.id, cus.company_id)
 		cus.orders.where(:status => ['completed', 'template']).order(created_at: :desc).each_with_index do |ord, odx|
+			next if template.nil? || template.id != ord.id
 			data = {
 				'ID' => cus.id,
 				'Name' => "#{cus.fname} #{cus.lname}",
@@ -34,7 +35,7 @@ def print_member_orders comp, session, sheet
 				'Import Scope' => 'Order',
 				'troly_customer_id' => cus.id,
 				'email' => cus.email,
-				'F' => '',
+				'membership' => cus.memberships.where(:status => 'current').last.name,
 				'gender' => '',
 				'cc_number' => '',
 				'cc_name' => '',
@@ -74,20 +75,23 @@ end
 
 def print_company_products comp, session, sheet
 	product_sheet = RakeHelper::init_google_worksheet "Product SKUs", sheet
-	comp.products.where(:archived_at => nil, :variety => [0, 2]).order(name: :asc).each_with_index do |prod, pdx|
+	comp.products.where(:archived_at => nil, :variety => [0, 2]).where.not(:product_number => nil).order(name: :asc).each_with_index do |prod, pdx|
 		data = {
 			'ID' => prod.id,
 			'Name' => prod.name,
 			'Vintage' => prod.vintage,
-			'SKU' => prod.product_number
+			'SKU' => prod.product_number,
+			'Price (Retail)' => prod.price,
+			'Price (6-pack)' => prod.price_6pk,
+			'Price (Case)' => prod.price_case
 		}
-		product_sheet = RakeHelper::set_wsheet_row(product_sheet, pdx, data, 0)
+		product_sheet = RakeHelper::set_wsheet_row(product_sheet, pdx, data, 1)
 		puts "Saved #{prod.name}"
 	end
 	product_sheet.save
 end
 
-company = RakeHelper::company_lookup
+company = RakeHelper::company_lookup 10225
 print_member_orders company, session, sheet
 print_company_products company, session, sheet
 
