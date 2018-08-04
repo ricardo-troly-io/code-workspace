d1849c19777ab98a780a54960659a6bb1451e2d7
Seb Tee <seb@empireone.com.au>
2018-08-04 11:58:17 +1000

    Added wine regions manipulations

---
+++ 2018-08 Wine Regions to AC.rb
@@ -0,0 +1,243 @@
+str=<<-WINE
+
+NSW
+NSW, Sydney
+NSW, New England
+NSW, Hastings River
+NSW, Hunter Valley
+NSW, Mudgee
+NSW, Orange
+NSW, Cowra
+NSW, Hilltops
+NSW, Southern Highlands
+NSW, Shoalhaven Coast
+NSW, Canberra District
+NSW, Gundagai
+NSW, Tumbarumba
+NSW, Riverina
+NSW, Murray Darling
+NSW, Pericoota
+
+VIC
+VIC, Murray Darling
+VIC, Swan Hill
+VIC, Goulburn Valley
+VIC, Strathbogie Ranges
+VIC, Heathcote
+VIC, Bendigo
+VIC, Pyrenees
+VIC, Rutherglen
+VIC, Gippsland
+VIC, Beechworth
+VIC, Alpine Valleys
+VIC, King Valley
+VIC, Glenrowan
+VIC, Upper Goulburn
+VIC, Melbourne
+VIC, Yarra Valley
+VIC, Mornington Peninsula
+VIC, Sunbury
+VIC, Geelong
+VIC, Macedon Ranges
+VIC, Grampians
+VIC, Henty
+
+
+QLD
+QLD, South Burnett
+QLD, Granite Belt
+
+SA
+SA, Adelaide
+SA, Southern Flinders Ranges
+SA, Clare Valley
+SA, Barossa Valley
+SA, Eden Valley
+SA, Adelaide Plains
+SA, Adelaide Hills
+SA, McLaren Vale
+SA, Langhorne Creej
+SA, Riverland
+SA, Kangaroo Island
+SA, Southern Fleurieu
+SA, Currency Creek
+SA, Padthaway
+SA, Wrattonbully
+SA, Mount Benson
+SA, Robe
+SA, Coonawarra
+SA, Mount Gambier
+
+WA
+WA, Swan District
+WA, Perth
+WA, Perth Hills
+WA, Peel
+WA, Geographe
+WA, Margaret River
+WA, Blackwood Valley
+WA, Pemberton
+WA, Manjimup
+WA, Great Southern
+
+TAS
+TAS, North
+TAS, South
+TAS, East
+
+CA, Redwoods
+CA, Redwoods - Willow Creak
+CA, Redwoods - Seiad Valley
+CA, Redwoods - Trinity Lakes
+
+CA, North Coast
+CA, North Coast - Mendocino
+CA, North Coast - Mendocino - Dos Rios
+CA, North Coast - Mendocino - Covelo
+CA, North Coast - Mendocino - Redwood Valley
+CA, North Coast - Mendocino - Potter Valley
+CA, North Coast - Mendocino - Anderson Valley
+CA, North Coast - Mendocino - Mendocino Ridge
+CA, North Coast - Mendocino - Yorkville Highlands
+
+CA, North Coast - Clear Lake
+CA, North Coast - Clear Lake - High Valley
+CA, North Coast - Clear Lake - Big Valley District
+CA, North Coast - Clear Lake - Kelsey Bench
+CA, North Coast - Clear Lake - Red Hills
+CA, North Coast - Clear Lake - Guenoc Valley
+
+CA, North Coast - Sonoma
+CA, North Coast - Sonoma - Dry Creek Valley
+CA, North Coast - Sonoma - Rockpile
+CA, North Coast - Sonoma - Dry Creek Valley
+CA, North Coast - Sonoma - Fort Ross / Seaview
+CA, North Coast - Sonoma - Alexander Valley
+CA, North Coast - Sonoma - Chalk Hill
+CA, North Coast - Sonoma - Knights Valley
+CA, North Coast - Sonoma - Russian River Valley
+CA, North Coast - Sonoma - Green Valley
+CA, North Coast - Sonoma - Sonoma Coast
+CA, North Coast - Sonoma - Bennett Valley
+CA, North Coast - Sonoma - Sonoma Mountain
+CA, North Coast - Sonoma - Moon Mountain
+CA, North Coast - Sonoma - Sonoma Valley
+CA, North Coast - Sonoma - Bennett Valley
+CA, North Coast - Sonoma - Carneros
+
+CA, North Coast - Napa Valley
+CA, North Coast - Napa Valley - Calistoga
+CA, North Coast - Napa Valley - Howell Mountain
+CA, North Coast - Napa Valley - Diamond Mountain
+CA, North Coast - Napa Valley - Spring Mountain
+CA, North Coast - Napa Valley - St. Helena
+CA, North Coast - Napa Valley - Rutherford
+CA, North Coast - Napa Valley - Oakville
+CA, North Coast - Napa Valley - Atlas Peak
+CA, North Coast - Napa Valley - Stage Leap District
+CA, North Coast - Napa Valley - Yountville
+CA, North Coast - Napa Valley - Oak Knoll District
+CA, North Coast - Napa Valley - Mount Veeder
+CA, North Coast - Napa Valley - Wild Horse Valley
+CA, North Coast - Napa Valley - Carneros
+CA, North Coast - Napa Valley - Coombsville
+CA, North Coast - Napa Valley - Chiles Valley
+
+CA, Inland Valleys
+CA, Inland Valleys - Madera
+CA, Inland Valleys - Lodi
+CA, Inland Valleys - Capay Valley
+CA, Inland Valleys - Dunnigan Hills
+CA, Inland Valleys - Clarksburg
+CA, Inland Valleys - Suisun Valley
+
+CA, Sierra Foothills
+CA, Sierra Foothills - Eldorado
+CA, Sierra Foothills - Shenandoah
+CA, Sierra Foothills - Fairplay
+CA, Sierra Foothills - Fiddletown
+CA, Sierra Foothills - North Yuba
+
+CA, Central Coast
+CA, Central Coast - San Francisco Bay
+CA, Central Coast - San Francisco Bay - Livermore Valley
+CA, Central Coast - San Francisco Bay - Santa Cruz Mountains
+CA, Central Coast - San Francisco Bay - Ben Lamond Mountain
+CA, Central Coast - San Francisco Bay - Son Ysidro District
+CA, Central Coast - San Francisco Bay - Pacheco Pass
+
+CA, Central Coast - San Benito 
+CA, Central Coast - San Benito - Pacheco Pass
+CA, Central Coast - San Benito - Cienega Valley
+CA, Central Coast - San Benito - Mount Harlan
+CA, Central Coast - San Benito - Lime Kiln Valley
+CA, Central Coast - San Benito - Paicines
+
+CA, Central Coast - Monterey
+CA, Central Coast - Monterey - Carmel Valley
+CA, Central Coast - Monterey - Santa Lucia Highlands
+CA, Central Coast - Monterey - Chalone
+CA, Central Coast - Monterey - Arroyo Seco
+CA, Central Coast - Monterey - San Bernabe
+CA, Central Coast - Monterey - San Lucas
+CA, Central Coast - Monterey - San Antonio Valley
+CA, Central Coast - Monterey - Hames Valley
+
+CA, Central Coast - Paso Robles
+CA, Central Coast - Paso Robles - San Miguel District
+CA, Central Coast - Paso Robles - Estrella District
+CA, Central Coast - Paso Robles - Geneseo District
+CA, Central Coast - Paso Robles - San Juran Creek
+CA, Central Coast - Paso Robles - Highlands District
+CA, Central Coast - Paso Robles - Creston District
+CA, Central Coast - Paso Robles - Adelaide District
+CA, Central Coast - Paso Robles - York Mountain
+CA, Central Coast - Paso Robles - Willo Creek District
+CA, Central Coast - Paso Robles - Templeton Gap District
+CA, Central Coast - Paso Robles - El Pomar District
+CA, Central Coast - Paso Robles - Santa Margarita Ranch
+
+CA, Central Coast - San Luis Obispo
+CA, Central Coast - San Luis Obispo - Edna Valley
+CA, Central Coast - San Luis Obispo - Arroyo Grande Valley
+
+CA, Central Coast - Santa Barbara
+CA, Central Coast - Santa Barbara - Santa Maria Valley
+CA, Central Coast - Santa Barbara - Sta Rita Hills
+CA, Central Coast - Santa Barbara - Santa Ynez Valley
+CA, Central Coast - Santa Barbara - Ballard Canyon
+CA, Central Coast - Santa Barbara - Happy Canyon
+
+CA, South Coast
+CA, South Coast - Malibu Coast
+CA, South Coast - Leona Valley
+CA, South Coast - Sierra Pelona Valley
+CA, South Coast - Cucamonga Valley
+CA, South Coast - Temecula Valley
+CA, South Coast - San Pasqual
+CA, South Coast - Ramona Valley
+
+WINE
+
+options = str.split("\n").select { |x| x.present? }.sort.map { |x| { "options[#{x}]" => x }}.reduce({}, :merge);
+
+params = {'id'=> 70, 'type' => 3, 'title' =>'Workshop Interest', 'prestag'=>'WORKSHOP_INTEREST', 'p[0]'=>0 };
+
+ac_post('list_field_edit',  params.merge(options) )
+
+
+
+str.split("\n").select { |x| x.present? }.sort.each do |x| 
+	puts <<-END
+
+        <div class="_row _checkbox-radio">
+          <input id="field_70#{x}" type="checkbox" name="field[70][]" value="#{x}"   >
+          <span>
+            <label for="field_70#{x}">
+              #{x}
+            </label>
+          </span>
+        </div>
+	END
+end;
+
